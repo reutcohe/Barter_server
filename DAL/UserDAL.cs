548c28d1a5e028e4635962a1b8ae4c7fb6694b3a
Chany87 <chany8708@gmail.com>
2022-11-21 20:09:26 +0200

    UserDAL

--- DAL/UserDAL.cs
+++ DAL/UserDAL.cs
@@ -1,4 +1,5 @@
 ﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,5 +39,50 @@ namespace DAL
             }
         }
 
+        public bool AddUser(User user)
+        {
+            try
+            {
+                bartersDBContext.Users.Add(user);
+                bartersDBContext.SaveChanges();
+                return true;
+            }
+            catch(Exception ex) {
+                throw ex;   
+            }
+        }
+        public bool DeleteUser(int id)
+        {
+            try
+            {
+                User user = bartersDBContext.Users.SingleOrDefault(x => x.Id == id); 
+                bartersDBContext.Users.Remove(user);
+                bartersDBContext.SaveChanges();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool UpdateUsers(int id, User user)
+        {
+            try
+            {
+                User user1 = bartersDBContext.Users.SingleOrDefault(x => x.Id == id);
+                bartersDBContext.Entry(user1).CurrentValues.SetValues(user);
+                bartersDBContext.SaveChanges();
+                return true;
+            }
+            catch(Exception ex)
+            { 
+
+                return false;
+            }
+        }
     }
 }
+
+
+
+
